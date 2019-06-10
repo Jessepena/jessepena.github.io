9c6d0cdee5b7ff3200e81671dff36bc64e283aaa
Jesse <jessepena@hotmail.es>
2019-06-10 18:04:25 -0400

    Added webpage files

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+window.addEventListener('load', function() {
+  var idToken;
+  var accessToken;
+  var expiresAt;
+
+  var webAuth = new auth0.WebAuth({
+    domain: 'dev-jesse.auth0.com',
+    clientID: 'G250VDJq023a250RBMI58p8TF34diYoJ',
+    responseType: 'token id_token',
+    scope: 'openid',
+    redirectUri: window.location.href
+  });
+
+  var loginBtn = document.getElementById('btn-login');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
